d62ff50ace2261995714dd2bc4de548c41b322a6
Mati <matiasfornes@gmail.com>
2020-06-29 12:27:50 +0200

    fix: remove compiled before compiling

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
+const fs = require('fs')
+
 const { getPage } = require('./generatePDF')
 const PDFMerger = require('./mergePDFs')
 const { getAll, index, root } = require('./pages')
 
+const compiledDocPath = './output/op-1_v2-2.pdf'
 /**
  * Generate a PDF manual for OP-1 from teenage engineering
  * Could be used for any other web site actually :D
  */
 ;(async () => {
+  try {
+    fs.unlinkSync(compiledDocPath)
+  } catch (_) {
+    console.info(`No need to remove ${compiledDocPath}.`)
+  }
+
   let order = 0
 
   await getPage(root + index, '000')
@@ -23,7 +32,7 @@ const { getAll, index, root } = require('./pages')
 
   merger.addAllFromFolder('./output/')
 
-  await merger.save('./output/op-1_v2-2.pdf')
+  await merger.save(compiledDocPath)
 
   process.exit(0)
 })()
